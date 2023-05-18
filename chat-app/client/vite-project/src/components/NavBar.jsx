3fb577aa02b0bd79143f98e14a4fd8ae95c27a99
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-18 11:06:57 +0530

    navbar

--- chat-app/client/vite-project/src/components/NavBar.jsx
+++ chat-app/client/vite-project/src/components/NavBar.jsx
@@ -1,5 +1,15 @@
+import { Container, Nav, Navbar, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const NavBar = () => {
-    return (  <>Nav</>);
+    return (<Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
+        <Container>
+            <h2>
+              <Link to="/">ChatApp</Link>  
+            </h2>
+        </Container>
+    </Navbar>
+    );
 }
  
 export default NavBar;
