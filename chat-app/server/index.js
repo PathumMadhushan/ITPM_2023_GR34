048a49898d194224e94aecd574f91d5d1670f303
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-18 22:26:29 +0530

    messagemodel

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const userRoute = require("./Routes/userRoute")
+const userRoute = require("./Routes/userRoute");
+const chatRoute = require("./Routes/chatRoute");
+
 
 const app = express();
-require("dotenv").config()
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
 
 app.get("/", (req, res) => {
     res.send("welcome our chat app APIs..");
 });
 
 
-const port = process.env.PORT || 7004;
+const port = process.env.PORT || 7006;
 const uri = process.env.ATLAS_URI;
 
 app.listen(port, (req, res) => {
