3024dbce539165dd72de83098137517914d8ac2a
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-18 12:47:50 +0530

    console

--- chat-app/client/vite-project/src/pages/Register.jsx
+++ chat-app/client/vite-project/src/pages/Register.jsx
@@ -4,7 +4,8 @@ import { AuthContext } from "../context/AuthContext";
 
 
 const Register = () => {
-  const {user}=  useContext(AuthContext)
+
+    const { registerInfo,updateRegisterInfo } = useContext(AuthContext);
     return (<>
         <Form>
             <Row style={{
@@ -15,11 +16,15 @@ const Register = () => {
                 <Col xs={6}>
                     <Stack gap={3}>
                         <h2>Register</h2>
-                         <h2>user.name</h2>
+                      
 
-                        <Form.Control type="text" placeholder="Name" />
-                        <Form.Control type="email" placeholder="Email" />
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="text" placeholder="Name" onChange={(e) => updateRegisterInfo
+                        ({...registerInfo,name:e.target.value})} />
+                        <Form.Control type="email" placeholder="Email" onChange={(e) => updateRegisterInfo
+                        ({...registerInfo,email:e.target.value})} />
+                        <Form.Control type="password" placeholder="Password" onChange={(e) => updateRegisterInfo
+                            ({ ...registerInfo, password: e.target.value })} />
+                        
                         <Button variant="primary" type="submit">
                             Register
                         </Button>
