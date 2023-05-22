424b26c5eb789629d09be10675da513ebe06597e
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:37:09 +0530

    ad d console user,logininfo

--- chat-app/client/vite-project/src/context/AuthContext.jsx
+++ chat-app/client/vite-project/src/context/AuthContext.jsx
@@ -18,4 +18,62 @@ export const AuthContextProvider = ({ children }) => {
         email: "",
         password: "",
     });
+
+    console.log("Userr", user);
+    console.log("loginInfo", loginInfo);
+
+    useEffect(() => {
+        const user = localStorage.getItem("User")
+        setUser(JSON.parse(user));
+    }, []);
+
+    const updateRegisterInfo = useCallback((info) => {
+        setRegisterInfo(info);
+    }, []);
+    const updateLoginInfo = useCallback((info) => {
+        setLoginInfo(info);
+    }, []);
+    
+    const registerUser = useCallback(async (e) => {
+        e.preventDefault();
+
+        setIsRegisterLoading(true);
+        setRegisterError(null);
+
+        const response = await postRequest(`${baseUrl}/users/register`, JSON.stringify
+            (registerInfo));
+        setIsRegisterLoading(false)
+        
+        if (response.error) {
+            return setRegisterError(response);
+        }
+
+        localStorage.setItem("User", JSON.stringify(response))
+        setUser(response);
+    }, [registerInfo]);
+
+    const loginUser = useCallback(async (e) => {
+        
+        e.preventDefault()
+
+        setIsLoginLoading(true)
+        setLoginError(null)
+
+        const response = await postRequest(
+            `${baseUrl}/users/login`,
+            JSON.stringify(loginInfo));
+        
+        setIsLoginLoading(false)
+        if (response.error) {
+            return setLoginError(response)
+        }
+        
+        localStorage.setItem("User", JSON.stringify(response))
+        setUser(response);
+    }, [loginInfo]);
+
+    const logoutUser = useCallback(() => {
+        localStorage.removeItem("User");
+        setUser(null);
+    }, [])
 }
