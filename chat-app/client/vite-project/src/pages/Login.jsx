a44396e0a177e8cea00cd39a7122ebf6292c8283
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:59:18 +0530

    const logininfo,updateinfo

--- chat-app/client/vite-project/src/pages/Login.jsx
+++ chat-app/client/vite-project/src/pages/Login.jsx
@@ -2,3 +2,10 @@ import { useContext } from "react";
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
+const Login = () => {
+    const { loginUser,
+        loginError,
+        loginInfo,
+        updateLoginInfo,
+        isLoginLoading } = useContext(AuthContext);
+}
