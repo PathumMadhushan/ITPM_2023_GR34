30b0920b1a57718fa41d65984766658841197b73
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:51:04 +0530

    import userchat

--- chat-app/client/vite-project/src/pages/Register.jsx
+++ chat-app/client/vite-project/src/pages/Register.jsx
@@ -1,3 +1,4 @@
 import { useContext } from "react";
 import { ChatContext } from "../context/ChatContext";
 import { Container, Stack } from "react-bootstrap";
+import UserChat from "../components/chat/UserChat";
