4dd0c5eb127100f72d070ebb15f76af1088c005a
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 23:21:41 +0530

    export baseurl

--- chat-app/client/vite-project/src/utils/services.js
+++ chat-app/client/vite-project/src/utils/services.js
@@ -1,1 +1,11 @@
 export const baseUrl = "http://localhost:8090/api";
+
+export const postRequest = async (url, body) => {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body,
+    });
+}
