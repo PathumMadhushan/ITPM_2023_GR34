b800efab827acc5a91a72f4db71c948728b962d6
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:28:15 +0530

    import authcontext

--- chat-app/client/vite-project/src/components/NavBar.jsx
+++ chat-app/client/vite-project/src/components/NavBar.jsx
@@ -1,2 +1,4 @@
 import { Container, Nav, Navbar, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
