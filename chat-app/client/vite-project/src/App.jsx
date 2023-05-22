29b327758000c1665ec7f6184a44a8c8d12eeb5b
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 23:33:31 +0530

    import bootsrap

--- chat-app/client/vite-project/src/App.jsx
+++ chat-app/client/vite-project/src/App.jsx
@@ -2,3 +2,4 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./pages/Chat";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
