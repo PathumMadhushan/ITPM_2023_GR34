fa709e9e392e1bc12e3e68c19886f2127bdcaba3
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:40:56 +0530

    export chatconextprovider

--- chat-app/client/vite-project/src/context/ChatContext.jsx
+++ chat-app/client/vite-project/src/context/ChatContext.jsx
@@ -2,3 +2,9 @@ import { createContext, useEffect, useState } from "react";
 import { baseUrl, getRequest,postRequest } from "../utils/services";
 
 export const ChatContext = createContext();
+
+export const ChatContextProvider = ({ children, user }) => {
+    const [userChats, setUserChats] = useState(null);
+    const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
+    const [userChatsError, setUserChatsError] = useState(null);
+}
