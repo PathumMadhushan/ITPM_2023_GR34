d851c466086a7ecf0cfc4b3382d9da10a68b443f
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:34:46 +0530

    ad const name,email,password

--- chat-app/client/vite-project/src/context/AuthContext.jsx
+++ chat-app/client/vite-project/src/context/AuthContext.jsx
@@ -2,3 +2,14 @@ import { createContext, useEffect, useState } from "react";
 import { useCallback } from "react";
 import { baseUrl, postRequest } from "../utils/services";
 export const AuthContext = createContext()
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [registerError, setRegisterError] = useState(null);
+    const [isRegisterLoading, setIsRegisterLoading] = useState(false);
+    const [registerInfo, setRegisterInfo] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+}
