f653b9ba6b9d4593c64178538647699515dfccbf
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:33:32 +0530

    import usecallback

--- chat-app/client/vite-project/src/context/AuthContext.jsx
+++ chat-app/client/vite-project/src/context/AuthContext.jsx
@@ -1,3 +1,4 @@
 import { createContext, useEffect, useState } from "react";
 import { useCallback } from "react";
 import { baseUrl, postRequest } from "../utils/services";
+export const AuthContext = createContext()
