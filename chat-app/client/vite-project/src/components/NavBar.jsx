ec0d75adda99f717079a0477aec7705fadec515c
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:29:17 +0530

    const navbar

--- chat-app/client/vite-project/src/components/NavBar.jsx
+++ chat-app/client/vite-project/src/components/NavBar.jsx
@@ -2,3 +2,8 @@ import { Container, Nav, Navbar, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
+
+const NavBar = () => {
+
+    const { user, logoutUser } = useContext(AuthContext);
+}
