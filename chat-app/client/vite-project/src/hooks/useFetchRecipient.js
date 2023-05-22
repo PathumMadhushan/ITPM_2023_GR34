be93de72b5c6c124c5160d99b277990582fd4bf8
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:46:57 +0530

    return recipientuser

--- chat-app/client/vite-project/src/hooks/useFetchRecipient.js
+++ chat-app/client/vite-project/src/hooks/useFetchRecipient.js
@@ -6,4 +6,23 @@ export const useFetchRecipientUser = (chat, user) => {
     const [error, setError] = useState(null)
     
     const recipientId = chat?.members.find((id) => id !== user?._id)
-}
+ useEffect(() => {
+        
+        const getUser = async () => {
+            
+            if (!recipientId) return null
+            const response = await getRequest(`${baseUrl}/users/find/$
+            {recipientId}`);
+
+            if (response.error) {
+                return setError(error);
+            }
+            setRecipientUser(response);
+
+        };
+        getUser();
+        
+    }, []);
+
+    return { recipientUser };
+};
