c1c940b78ccabd9007d8545a5e451143e3497506
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-22 22:56:42 +0530

    create const register

--- chat-app/client/vite-project/src/pages/Register.jsx
+++ chat-app/client/vite-project/src/pages/Register.jsx
@@ -1,3 +1,8 @@
 import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+
+const Register = () => {
+
+}
+        
