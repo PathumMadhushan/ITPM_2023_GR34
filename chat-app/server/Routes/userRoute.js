3b4967373829815be01338084a769987dbab5d0a
Yashodha99 <101858881+Yashodha99@users.noreply.github.com>
2023-05-23 09:30:24 +0530

    api registeruser

--- chat-app/server/Routes/userRoute.js
+++ chat-app/server/Routes/userRoute.js
@@ -2,3 +2,5 @@ const express = require("express");
 const { registerUser, loginUser, findUser, getUsers } = require("../Controllers/userController");
 
 const router = express.Router();
+
+router.post("/register", registerUser);
